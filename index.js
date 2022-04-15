1498a04ba870994408507505c82279d4636c586e
Igor <i.sulialev@mail.ru>
2022-04-15 17:47:54 +0300

    fix by eslint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-console.log("Hello, Hexlet!");
-console.log("Aaagh!");
-import _ from "lodash";
-console.log(_.last(["one", "two"]));
-const a = 5 + 1;
+import _ from 'lodash';
+
+console.log('Hello, Hexlet!');
+console.log('Aaagh!');
+console.log(_.last(['one', 'two']));
